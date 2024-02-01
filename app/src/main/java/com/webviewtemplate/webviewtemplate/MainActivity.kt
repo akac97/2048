8ca502e9a1708329b90a5f0a1f32a0933d335411
akac97 <142359207+akac97@users.noreply.github.com>
2024-02-01 19:16:43 +0100

    Update MainActivity.kt

--- app/src/main/java/com/webviewtemplate/webviewtemplate/MainActivity.kt
+++ app/src/main/java/com/webviewtemplate/webviewtemplate/MainActivity.kt
@@ -3,12 +3,17 @@ package com.webviewtemplate.webviewtemplate
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
+import android.os.Bundle
 import android.webkit.WebView
+import android.window.OnBackInvokedDispatcher
 import com.webviewtemplate.webviewtemplate.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
-    // Load local HTML file from the 'assets' directory
+    // you can make offline application with local file
     private val applicationUrl = "file:///android_asset/index.html"
+    //or you can load url
+    //private val applicationUrl ="https://purnorup.com/challenging-bricks"
+    //private val applicationUrl = "https://www.wikipedia.org/"
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
 
@@ -18,6 +23,7 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         webView = binding.webView
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             onBackInvokedDispatcher.registerOnBackInvokedCallback(
@@ -31,9 +37,12 @@ class MainActivity : Activity() {
             }
         }
 
+
         webView.settings.domStorageEnabled = true
         webView.settings.javaScriptEnabled = true
 
+
         webView.loadUrl(applicationUrl)
     }
+
 }
