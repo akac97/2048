dba3118874f6571531f25ddaff6af5816fa63b9f
akac97 <142359207+akac97@users.noreply.github.com>
2024-02-01 18:14:12 +0100

    Update MainActivity.kt

--- app/src/main/java/com/webviewtemplate/webviewtemplate/MainActivity.kt
+++ app/src/main/java/com/webviewtemplate/webviewtemplate/MainActivity.kt
@@ -10,7 +10,7 @@ import com.webviewtemplate.webviewtemplate.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
     // you can make offline application with local file
-    private val applicationUrl = "file://assets/index.html"
+    private val applicationUrl = "file:///assets/index.html"
     //or you can load url
     //private val applicationUrl ="https://purnorup.com/challenging-bricks"
     //private val applicationUrl = "https://www.wikipedia.org/"
